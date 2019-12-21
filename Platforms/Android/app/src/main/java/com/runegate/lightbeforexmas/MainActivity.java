237be9d11b6415d64c82fa98c5c0eabade16ace4
Abe Pralle <abe.pralle@gmail.com>
2019-12-21 00:33:19 -0800

    Running on Android

---
+++ Platforms/Android/app/src/main/java/com/runegate/lightbeforexmas/MainActivity.java
@@ -0,0 +1,41 @@
+package com.runegate.lightbeforexmas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+import org.plasmacore.*;
+
+public class MainActivity extends Activity
+{
+  public PlasmacoreView plasmacoreView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+
+    // Turn off title bar
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    plasmacoreView = PlasmacoreView.builder(this).build();
+    setContentView( plasmacoreView );
+  }
+
+  @Override
+  protected void onPause()
+  {
+    super.onPause();
+    plasmacoreView.onPause();
+  }
+
+  @Override
+  protected void onResume()
+  {
+    super.onResume();
+    plasmacoreView.onResume();
+  }
+}
+
